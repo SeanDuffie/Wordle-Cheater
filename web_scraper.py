750fee59d652808d2d90fa75d5ad0a66e495dda7
SeanDuffie <duffiesean@gmail.com>
2024-02-28 18:10:47 -0500

    Updated webscraper to output to the right location Output csv is cleaner Old words file is no longer needed and was removed

--- web_scraper.py
+++ web_scraper.py
@@ -1,19 +1,42 @@
 """_summary_
     https://www.wordunscrambler.net/word-list/wordle-word-list
 """
+import os
+
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+RTDIR = os.path.dirname(__file__)
+
 def scrape_website(url: str) -> requests.models.Response:
+    """_summary_
+
+    Args:
+        url (str): _description_
+
+    Returns:
+        requests.models.Response: _description_
+    """
+    # Perform the request
     response = requests.get(url)
 
+    # Check if the HTTP request was successful
     if response.status_code == 200:
         return response
     else:
         print('Failed to retrieve BBC News.')
-        return(None)
+        return None
 
-def parse_html(html: requests.models.Response) -> list:
+def parse_html(html: requests.models.Response) -> pd.Series:
+    """_summary_
+
+    Args:
+        html (requests.models.Response): _description_
+
+    Returns:
+        pd.Series: _description_
+    """
     # Create the HTML Text Parser
     soup = BeautifulSoup(html.text, 'html.parser')
     # Find all links that contain an href
@@ -23,11 +46,15 @@ def parse_html(html: requests.models.Response) -> list:
     option_list = []
     for link in links:
         if "/unscramble/" in link['href']:
+            # option_list.append(link.text)
             option_list.append(link.text)
 
-    return option_list
+    options = pd.Series(option_list)
+    return options
 
 if __name__ == "__main__":
     URL = 'https://www.wordunscrambler.net/word-list/wordle-word-list'
     HTML = scrape_website(url=URL)
-    parse_html(html=HTML)
+    db = parse_html(html=HTML)
+    db.to_csv(path_or_buf=f"{RTDIR}/words.csv", index=False, header=False)
+    print(db)
