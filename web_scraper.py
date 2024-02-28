bc223de34f10f92c8decffabdd9a531d693b0e4a
SeanDuffie <duffiesean@gmail.com>
2024-02-28 17:37:07 -0500

    Greatly improved the Webscraper Can now make a new wordle database

--- web_scraper.py
+++ web_scraper.py
@@ -4,30 +4,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-def scrape_website(url):
+def scrape_website(url: str) -> requests.models.Response:
     response = requests.get(url)
 
     if response.status_code == 200:
-        print(type(response))
         return response
     else:
         print('Failed to retrieve BBC News.')
         return(None)
-    
-def parse_html(html):
-    soup = BeautifulSoup(html.text, 'html.parser')
-    headlines = soup.find_all('h3', class_='list-header')
 
-    for headline in headlines:
-        # headline_text = headline.text.strip()
-        print(headline.text.strip())
-        marker = headline
-        while marker is not None:
-            marker = marker.find_next('a')
-            print(marker.text)
+def parse_html(html: requests.models.Response) -> list:
+    # Create the HTML Text Parser
+    soup = BeautifulSoup(html.text, 'html.parser')
+    # Find all links that contain an href
+    links = soup.find_all('a', href=True)
+
+    # For each link, check whether it contains a Wordle option
+    option_list = []
+    for link in links:
+        if "/unscramble/" in link['href']:
+            option_list.append(link.text)
+
+    return option_list
 
 if __name__ == "__main__":
     URL = 'https://www.wordunscrambler.net/word-list/wordle-word-list'
     HTML = scrape_website(url=URL)
     parse_html(html=HTML)
-    
