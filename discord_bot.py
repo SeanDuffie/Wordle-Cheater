d64fd0c546856389ab659586cce319ce95297251
SeanDuffie <duffiesean@gmail.com>
2024-05-01 18:06:05 -0400

    Added template for new discord bot

---
+++ discord_bot.py
@@ -0,0 +1,29 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("The bot is ready!")
+    await client.change_presence(game=discord.Game(name="Crushing today's Wordle"))
+
+@client.event
+async def on_message(message):
+    # Bot shouldn't be responding to itself
+    if message.author == client.user:
+        return
+    if message.content == "Hello":
+        await client.send_message(message.channel, "World")
+
+client.run(TOKEN)
+
+
+""" Features
+    - Automatically posts bot score from each wordle day
+    - Allows people to request a new word
+        - Users can guess and have results delivered back
+        - User's guess is hidden, but results are visible
+        - User results are specific to each person
+        - Winner per day is recorded
+        - Person with the most wins is given a new role
+"""
