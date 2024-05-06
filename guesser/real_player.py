256910ed9c36d12dc44ee0f7158d2851c24639c5
SeanDuffie <duffiesean@gmail.com>
2024-05-06 18:55:17 -0400

    Added ability to read in final solution from NYT if guessing failed. Added WordBank integrations. Added documentation on all functions.

--- guesser/real_player.py
+++ guesser/real_player.py
@@ -9,10 +9,18 @@ import time
 import selenium.webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from word_bank import WordBank
 
 
 class RealPlayer():
-    """_summary_
+    """ RealPlayer is a slightly more advanced webscraper than my previous one.
+
+        This object will allow the user to launch a simulated test browser in chrome,
+        click buttons, and then send keypresses to simulate entering words. This will
+        also allow the user to query the results of each guess from the NYT webpage.
+
+        This object is also designed to play nicely with both the WordBank object and
+        the DiscordBot interfaces.
     """
     def __init__(self, url: str):
         # Launch the Chrome browser
@@ -37,30 +45,59 @@ class RealPlayer():
         self.driver.close()
         self.driver.quit()
 
-    def select_button(self, method: str, value: str):
+    def select_button(self, method: str, value: str) -> None:
+        """ Click an element on the webpage
+
+        Used to locate and close dialog/tutorial boxes when they pop up.
+
+        Args:
+            method (str): What method to use to find it? Class name, xpath, id, etc.
+            value (str): What is the value to search for?
+        """
         self.driver.find_element(by=method, value=value).click()
         time.sleep(.1)
 
-    def play_word(self, word: str):
+    def play_word(self, word: str) -> str:
+        """ Plays a word on the NYT Wordle webpage by typing it.
+
+        Args:
+            word (str): What word to play?
+
+        Returns:
+            str: Results of that word from the site itself
+        """
+        # Handle going over the guess limit
         if self.counter >= 6:
-            print("Over Guess Limit")
-            # yield "loser"
+            # Instead of returning nothing, we will go over the guess limit by one to get the actual answer
+            result = self.driver.find_element(by=By.XPATH, value='//*[@id="ToastContainer-module_gameToaster__HPkaC"]/div').text.lower()
         else:
-            print(f"Sending word: {word}")
+            # Send the keypresses to the webpage
+            self.actions.send_keys(word + "\n")
+            self.actions.perform()
 
-        self.actions.send_keys(word + "\n")
-        self.actions.perform()
+            # Give enough time for the animation to finish, then read the results
+            time.sleep(2)
+            result = self.read_results(self.counter)
 
-        # TODO: yield results
-        time.sleep(2)
-        result = self.read_results(self.counter)
-
-        # Increment
-        self.counter += 1
+            # Increment guess counter
+            self.counter += 1
 
         return result
 
-    def read_results(self, row: int):
+    def read_results(self, row: int) -> str:
+        """ Read the results by scanning the table
+
+        FIXME: Handle if user requests a row that is not populated yet
+            - Try/except?
+            - What to return?
+            - 
+
+        Args:
+            row (int): Which row should you read? (This won't work if there are no results yet)
+
+        Returns:
+            str: The results parsed into a WordBank friendly string
+        """
         result = ""
         div_list = self.driver.find_elements(by=By.CLASS_NAME, value="Tile-module_tile__UWEHN")
 
@@ -78,19 +115,27 @@ class RealPlayer():
 
         return result
 
-if __name__ == "__main__":
-    URL = "https://www.nytimes.com/games/wordle/index.html"
+def run():
+    """ Main runner for RealPlayer """
+    url = "https://www.nytimes.com/games/wordle/index.html"
+    wb = WordBank()
+    guess = "flash"
+    history = []
 
-    with RealPlayer(URL) as player:
-        # Wait for the page to load and animations to finish, then
-        print(player.play_word("flash"))
-        print(player.play_word("flash"))
-        print(player.play_word("flash"))
-        print(player.play_word("flash"))
-        print(player.play_word("flash"))
-        print(player.play_word("flash"))
+    with RealPlayer(url=url) as player:
+        while True:
+            result = player.play_word(guess)
+            history.append((guess, result))
+            print(history)
 
-        # TODO: if failed, read the correct answer
+            if result == "22222" or result.isalpha():
+                break
+
+            guess = wb.submit_guess(word=guess, res=result, method="slo")0
         time.sleep(3)
 
     print("Done")
+    return history
+
+if __name__ == "__main__":
+    run()
