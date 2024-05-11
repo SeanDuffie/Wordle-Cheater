b4cb2646fc7a402aefe2c2a72a4b580de91a126b
SeanDuffie <sean.duffie@tamu.edu>
2024-05-11 19:19:56 -0400

    Added a task to restart everything at midnight Added storage for most recent solutions Added automatic processing of user !play

--- guesser/discord_bot.py
+++ guesser/discord_bot.py
@@ -20,6 +20,7 @@
 """
 import datetime
 import os
+import random
 
 import discord
 import discord.ext
@@ -29,7 +30,7 @@ import pandas as pd
 from dotenv import load_dotenv
 from real_player import RealPlayer
 from word_bank import WordBank
-from typing import Dict
+from typing import Dict, List, Tuple
 
 # Set Discord intents (these are permissions that determine what the bot is allowed to observe)
 intents = discord.Intents.default()
@@ -38,8 +39,12 @@ intents.message_content = True
 
 ############### SET TIME HERE ###############
 LOCAL_TZ = datetime.datetime.now().astimezone().tzinfo
-SCH_TM = datetime.time(6, tzinfo=LOCAL_TZ)
-BONUS_SCH = [datetime.time(12, tzinfo=LOCAL_TZ), datetime.time(6, tzinfo=LOCAL_TZ)]
+TIMES = [
+    datetime.time(6, tzinfo=LOCAL_TZ),
+    datetime.time(12, tzinfo=LOCAL_TZ),
+    datetime.time(18, tzinfo=LOCAL_TZ),
+    datetime.time(0, tzinfo=LOCAL_TZ)
+]
 #############################################
 
 ############ GET ENVIRONMENT VARIABLES ############
@@ -52,7 +57,8 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 # Default Channel to execute Wordle commands in
 CTX = None
 DF = pd.DataFrame(columns=["Time", "User", "Times Played", "Average Score", "Success Ratio", "Bot Win Ratio", "Guess 1", "Guess 2", "Guess 3", "Guess 4", "Guess 5", "Guess 6"])
-banks: Dict[discord.User, WordBank] = {}
+banks: Dict[discord.User, List[str]] = {}
+solutions: List[str] = ["", "", ""]
 
 # NOTE: I use commands.Bot because it extends features of the Client to allow things like commands
 # Initialize Discord Bot
@@ -61,6 +67,41 @@ wordle_bot = discord.ext.commands.Bot(
     intents=intents,
     help_command=discord.ext.commands.HelpCommand()
 )
+
+
+def check(guess: str, solution: str) -> str:
+    """ Generates a 'results' string from a guess when the solution is known
+
+    Args:
+        guess (str): the user or system generated guess
+        solution (str): the known solution to the current puzzel iteration
+
+    Returns:
+        str: Formatted results string that compares the solution with the guess
+    """
+    result = "00000"
+
+    # First, eliminate all letters known to be correct, mask the correct letters with whitespace
+    for i in range(5):
+        if guess[i] == solution[i]:
+            result = result[:i] + "2" + result[i+1:]
+            guess = guess[:i] + " " + guess[i+1:]
+            solution = solution[:i] + " " + solution[i+1:]
+        # print(f"{guess=} | {solution=} | {result=}")
+
+    # Next, identify all the characters that are present but not in the correct spot
+    # Doing it this way allows you to identify duplicate letters without looping on confirmed
+    # Additionally, only one "possible" duplicate letter should be marked for each duplicate
+    for i in range(5):
+        if guess[i] != " " and guess[i] in solution:
+            result = result[:i] + "1" + result[i+1:]
+
+            # Replace the character in solution with whitespace to handle duplicates
+            j = solution.index(guess[i])
+            solution = solution[:j] + " " + solution[j+1:]
+        # print(f"{guess=} | {solution=} | {result=}")
+
+    return result
 
 @wordle_bot.command()
 async def wordle(ctx: discord.ext.commands.context.Context):
@@ -75,32 +116,64 @@ async def wordle(ctx: discord.ext.commands.context.Context):
     response = f"Wordle {number:,} #\n\n"
     guess_count = 0
 
+    # while True:
+    #     try:
     with RealPlayer(url) as rp:
         for item in rp.run_generator():
             line = item[1].replace("2", ":green_square:").replace("1", ":yellow_square:").replace("0", ":black_large_square:")
             response += line + "\n"
             guess_count += 1
 
+            if item[1] == "22222" or item[1].isalpha():
+                solutions[0] = item[0]
+                print(solutions)
+
         if guess_count > 6:
             guess_count = "x"
         response = response.replace("#", f"{guess_count}/6")
+        #     break
+        # except:
+        #     pass
 
     await ctx.send(response)
 
 
 @wordle_bot.command()
 async def play(ctx: discord.ext.commands.context.Context, word: str, mode: int = 0):
+    # Check message for errors
+    try:
+        assert word.isalpha()
+        assert len(word) == 5
+    except AssertionError:
+        await ctx.reply("Invalid Guess")
+    # Check mode for errors
+    try:
+        assert mode >= 0
+        assert mode <= 3
+    except AssertionError:
+        await ctx.reply("Invalid Mode")
     # Read user input
+    if ctx.author not in banks:
+        banks[ctx.author] = []
 
-    # Delete user message
+    if len(banks[ctx.author]) > 6:
+        await ctx.reply("You've exceeded your maximum guesses :(")
+        # Delete user message
+    else:
+        banks[ctx.author].append(word)
+        print(banks[ctx.author])
 
-    # Send ephemeral message output
-    await ctx.send(f"You played {word}! Only you can see this...", mention_author=True, ephemeral=True)
+        # Send ephemeral message output
+        await ctx.reply(f"You played {word}! Only you can see this...", ephemeral=True)
 
-    # Send public message of results
-    results = "21012"
-    line = results.replace("2", ":green_square:").replace("1", ":yellow_square:").replace("0", ":black_large_square:")
-    await ctx.send(f"{ctx.author.mention} Wordle # | Guess #: {line}")
+        result = check(guess=word, solution=solutions[mode])
+
+        # Send public message of results
+        result = result.replace("2", ":green_square:").replace("1", ":yellow_square:").replace("0", ":black_large_square:")
+        await ctx.send(f"{ctx.author.mention} Wordle  | Guess {len(banks[ctx.author])}: {result}")
+
+    # # Delete user message
+    # await ctx.message.delete()
 
 @wordle_bot.command()
 async def stats(ctx: discord.ext.commands.context.Context, user: discord.User=None):
@@ -117,7 +190,7 @@ async def challenge(ctx: discord.ext.commands.context.Context, user: discord.Use
     await ctx.send(f"{ctx.author.mention} is challenging {user.mention}!")
 
 
-@discord.ext.tasks.loop(time=SCH_TM)
+@discord.ext.tasks.loop(time=TIMES[0])
 async def wordle_task():
     """ Play todays Wordle every day at 8am """
     url = "https://www.nytimes.com/games/wordle/index.html"
@@ -132,6 +205,9 @@ async def wordle_task():
             response += line + "\n"
             guess_count += 1
 
+            if item[1] == "22222" or item[1].isalpha():
+                solutions[0] = item[0]
+
         if guess_count > 6:
             guess_count = "x"
         response = response.replace("#", f"{guess_count}/6")
@@ -139,6 +215,71 @@ async def wordle_task():
     for channel in wordle_bot.get_all_channels():
         if channel.name.lower() in ["wordle", "worldle", "nyt"]:
             await channel.send(response)
+
+@discord.ext.tasks.loop(time=TIMES[1])
+async def wordle_noon():
+    """ Play todays Wordle every day at 8am """
+    url = "https://www.nytimes.com/games/wordle/index.html"
+    first = datetime.date(2021, 6, 19)
+    number = (datetime.date.today() - first).days
+    response = f"Wordle {number:,} #\n\n"
+    guess_count = 0
+
+    wb = WordBank()
+    solutions[1] = wb.word_bank["Words"][random.randint(0,len(wb.word_bank["Words"]))]
+
+    with RealPlayer(url) as rp:
+        for item in rp.run_generator():
+            line = item[1].replace("2", ":green_square:").replace("1", ":yellow_square:").replace("0", ":black_large_square:")
+            response += line + "\n"
+            guess_count += 1
+
+        if guess_count > 6:
+            guess_count = "x"
+        response = response.replace("#", f"{guess_count}/6")
+
+    for channel in wordle_bot.get_all_channels():
+        if channel.name.lower() in ["wordle", "worldle", "nyt"]:
+            await channel.send(response)
+
+@discord.ext.tasks.loop(time=TIMES[2])
+async def wordle_evening():
+    """ Play todays Wordle every day at 8am """
+    url = "https://www.nytimes.com/games/wordle/index.html"
+    first = datetime.date(2021, 6, 19)
+    number = (datetime.date.today() - first).days
+    response = f"Wordle {number:,} #\n\n"
+    guess_count = 0
+
+    wb = WordBank()
+    solutions[2] = wb.word_bank["Words"][random.randint(0,len(wb.word_bank["Words"]))]
+
+    with RealPlayer(url) as rp:
+        for item in rp.run_generator():
+            line = item[1].replace("2", ":green_square:").replace("1", ":yellow_square:").replace("0", ":black_large_square:")
+            response += line + "\n"
+            guess_count += 1
+
+        if guess_count > 6:
+            guess_count = "x"
+        response = response.replace("#", f"{guess_count}/6")
+
+    for channel in wordle_bot.get_all_channels():
+        if channel.name.lower() in ["wordle", "worldle", "nyt"]:
+            await channel.send(response)
+
+@discord.ext.tasks.loop(time=TIMES[3])
+async def reset():
+    """ Resets everything at midnight """
+    first = datetime.date(2021, 6, 19)
+    number = (datetime.date.today() - first).days
+
+    for channel in wordle_bot.get_all_channels():
+        if channel.name.lower() in ["wordle", "worldle", "nyt"]:
+            await channel.send(f"Resetting the Wordle for Day {number:,}")
+
+    global banks
+    banks = {}
 
 # @discord.ext.tasks.loop(time=BONUS_SCH)
 # async def bonus_wordle_task():
