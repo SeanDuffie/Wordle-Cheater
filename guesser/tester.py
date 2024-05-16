c7c314b2924a2278260a20705cabd6e813030b9f
SeanDuffie <duffiesean@gmail.com>
2024-05-16 17:16:59 -0400

    Partial tester changes

--- guesser/tester.py
+++ guesser/tester.py
@@ -1,4 +1,6 @@
-"""_summary_
+""" @file sim_player.py
+    @author Sean Duffie
+    @brief Simulates a player with a known input. This can be done either for statistics or just simulating a playthrough.
 
 TODO: Test if statistics are based on number of results left rather than letters eliminated
 TODO: Test switching method mid round
@@ -9,7 +11,7 @@ TODO: https://stackoverflow.com/questions/53249133/check-if-a-pattern-is-in-a-li
 import datetime
 import os
 import random
-from typing import Literal
+from typing import Generator, Literal, Tuple
 
 import numpy as np
 import pandas as pd
@@ -18,70 +20,101 @@ from word_bank import WordBank
 RTDIR = os.path.dirname(__file__)
 
 
-def check(guess: str, solution: str) -> str:
-    """ Generates a 'results' string from a guess when the solution is known
-
-    Args:
-        guess (str): the user or system generated guess
-        solution (str): the known solution to the current puzzel iteration
-
-    Returns:
-        str: Formatted results string that compares the solution with the guess
+class SimPlayer:
+    """ SimPlayer will be what gathers the statistical data from performance testing
     """
-    result = "00000"
+    def __init__(self, solution: str = None, method: str = "slo") -> None:
+        self.wb = WordBank()
 
-    # First, eliminate all letters known to be correct, mask the correct letters with whitespace
-    for i in range(5):
-        if guess[i] == solution[i]:
-            result = result[:i] + "2" + result[i+1:]
-            guess = guess[:i] + " " + guess[i+1:]
-            solution = solution[:i] + " " + solution[i+1:]
-        # print(f"{guess=} | {solution=} | {result=}")
+        if solution is None:
+            solution = self.wb.get_rand()
 
-    # Next, identify all the characters that are present but not in the correct spot
-    # Doing it this way allows you to identify duplicate letters without looping on confirmed
-    # Additionally, only one "possible" duplicate letter should be marked for each duplicate
-    for i in range(5):
-        if guess[i] != " " and guess[i] in solution:
-            result = result[:i] + "1" + result[i+1:]
+        assert solution in self.wb.original_bank["Words"]
+        self.solution = solution
 
-            # Replace the character in solution with whitespace to handle duplicates
-            j = solution.index(guess[i])
-            solution = solution[:j] + " " + solution[j+1:]
-        # print(f"{guess=} | {solution=} | {result=}")
+        assert method in ["cum", "uni", "slo", "tot"]
+        self.method = method
 
-    return result
+    def __enter__(self):
+        return self
 
-class Tester:
-    """ Tester will be what gathers the statistical data from performance testing
-    """
-    def __init__(self) -> None:
-        self.word_options = self.read_file()
+    def __exit__(self, exception_type, exception_value, exception_traceback):
+        pass
 
-    def read_file(self):
-        """ Reads in the word bank downloaded from the interned, then parses for only valid words
+    def read_results(self, guess: str) -> str:
+        """ Generates a 'results' string from a guess when the solution is known
+
+        Args:
+            guess (str): the user or system generated guess
+            solution (str): the known solution to the current puzzel iteration
 
         Returns:
-            pd.Dataframe: Single column Dataframe that has all possible 5 letter words
+            str: Formatted results string that compares the solution with the guess
         """
-        def valid_word(word: str):
-            """ Helper function intended to be "apply()"'d on a pandas dataframe column
+        result = "00000"
 
-            Checks to make sure that a string contains only letters and is 5 long
+        # First, eliminate all letters known to be correct, mask the correct letters with whitespace
+        for i in range(5):
+            if guess[i] == self.solution[i]:
+                result = result[:i] + "2" + result[i+1:]
+                guess = guess[:i] + " " + guess[i+1:]
+                self.solution = self.solution[:i] + " " + self.solution[i+1:]
+            # print(f"{guess=} | {self.solution=} | {result=}")
 
-            Args:
-                word (str): input string
+        # Next, identify all the characters that are present but not in the correct spot
+        # Doing it this way allows you to identify duplicate letters without looping on confirmed
+        # Additionally, only one "possible" duplicate letter should be marked for each duplicate
+        for i in range(5):
+            if guess[i] != " " and guess[i] in self.solution:
+                result = result[:i] + "1" + result[i+1:]
 
-            Returns:
-                bool: True if the word is 5 letters and contains no numbers or special characters
-            """
-            if len(str(word)) == 5:
-                return word.isalpha()
-            return False
+                # Replace the character in self.solution with whitespace to handle duplicates
+                j = self.solution.index(guess[i])
+                self.solution = self.solution[:j] + " " + self.solution[j+1:]
+            # print(f"{guess=} | {self.solution=} | {result=}")
 
-        file = pd.read_csv(filepath_or_buffer=f'{RTDIR}\\..\\valid_guesses.csv', names=["Words"])
-        mask = file["Words"].apply(valid_word)
-        return file[mask].reset_index(drop=True)
+        return result
+
+    def run_generator(self, first: str = "flash", method: Literal['cum', 'uni', 'slo', 'tot'] = None) -> Generator[Tuple[str, str], None, None]:
+        """ Main runner for RealPlayer """
+        if method is not None:
+            assert method in ["cum", "uni", "slo", "tot"]
+            self.method = method
+
+        assert len(first) == 5
+        assert first.isalpha()
+        assert first in self.wb.original_bank["Words"]
+        guess = first
+
+        while True:
+            try:
+                # Submit guess, then yield results
+                result = self.read_results(guess=guess)
+
+                # Check that the guess was accepted. TODO: If not, suggest a new one and continue.
+                if result is None:
+                    guess = "flash"
+                    continue
+
+                # Yield Generator output
+                response = yield (guess, result)
+                if response is not None:
+                    assert len(response) == 5
+                    assert first.isalpha()
+                    assert first in self.wb.original_bank["Words"]
+                guess = guess if response is None else response
+
+                # Check victory conditions, or if out of guesses, get the final result
+                if result == "22222" or result.isalpha():
+                    return
+
+                # Get suggestion from the wordbank for the next guess
+                guess = self.wb.submit_guess(word=guess, res=result, method="slo")
+            except AssertionError:
+                if guess.lower() == "failed":
+                    print("Error! Word Bank ran out of options! (This shouldn't be possible)")
+                    return
+                print(f"Invalid Guess: {guess}")
 
     # TODO: FIXME: Eventually change the typehinting for method to a more sophisticated dict or other typehint method
     def play(self, start: str = "crane", solution: str = None, method: Literal['cum', 'uni', 'slo', 'tot'] = 'tot',
@@ -160,65 +193,69 @@ class Tester:
         # FIXME: Is guess_count necessary now that the guesses are logged as a list?
         return guess_count, guesses
 
-    def permutations(self, method: Literal['cum', 'uni', 'slo', 'tot'] = 'tot'):
-        """ Runs through all the permutations of starting word compared to solution
+def permutate(method: Literal['cum', 'uni', 'slo', 'tot'] = 'tot'):
+    """ Runs through all the permutations of starting word compared to solution
 
-            All other logic should be handled in the WordBank class
-            TODO: Add multiprocessing here for faster runtimes
-        """
+        All other logic should be handled in the WordBank class
+        TODO: Add multiprocessing here for faster runtimes
+    """
+    wb = WordBank()
 
-        other_headers = ["Time", "Start", "Average Score", "Min Score", "Max Score", "Failure Count", "Failures"]
-        df2 = pd.DataFrame(columns=other_headers)
+    other_headers = ["Time", "Start", "Average Score", "Min Score", "Max Score", "Failure Count", "Failures"]
+    df2 = pd.DataFrame(columns=other_headers)
 
-        # Loop through all starting words
-        time_start_perm = datetime.datetime.now()
-        for start_word in ['flash']: # self.word_options["Words"]: # ['flash', 'caste', 'crane', 'worst']: #
-            headers = ["Word", "Count"]
-            df = pd.DataFrame(columns=headers)
-            # TODO: Add multiprocessing here
-            row = [start_word]
-            failed = []
+    # Loop through all starting words
+    time_start_perm = datetime.datetime.now()
+    for start_word in ['flash']: # self.word_options["Words"]: # ['flash', 'caste', 'crane', 'worst']: #
+        headers = ["Word", "Count"]
+        df = pd.DataFrame(columns=headers)
+        # TODO: Add multiprocessing here
+        row = [start_word]
+        failed = []
 
-            time_start_word = datetime.datetime.now()
-            # Loop through all potential solutions
-            for solution in self.word_options["Words"]:
-                count, guesses = self.play(start=start_word, solution=solution, manual=False, method=method)
+        time_start_word = datetime.datetime.now()
+        # Loop through all potential solutions
+        for solution in wb.original_bank():
+            count, guesses = simulate(start=start_word, solution=solution, manual=False, method=method)
 
-                row.append(count)
-                df.loc[len(df.index)] = [solution, count]
+            row.append(count)
+            df.loc[len(df.index)] = [solution, count]
 
-                if count > 6:
-                    failed.append((solution, count, guesses))
+            if count > 6:
+                failed.append((solution, count, guesses))
 
-            df.to_csv(path_or_buf=f"{RTDIR}/../data/permutations_{method}_{start_word}_full.csv", index=False)
-            time_stop_word = datetime.datetime.now()
-            rrow = np.array(row[1:])
-            word_time = time_stop_word-time_start_word
-            print(f"Took {word_time} seconds to process {start_word}")
-            print(f"{start_word} scored an average of {rrow.mean()} and failed {len(failed)} times")
+        df.to_csv(path_or_buf=f"{RTDIR}/../data/permutations_{method}_{start_word}_full.csv", index=False)
+        time_stop_word = datetime.datetime.now()
+        rrow = np.array(row[1:])
+        word_time = time_stop_word-time_start_word
+        print(f"Took {word_time} seconds to process {start_word}")
+        print(f"{start_word} scored an average of {rrow.mean()} and failed {len(failed)} times")
 
-            row2 = [word_time, start_word, rrow.mean(), rrow.min(), rrow.max(), len(failed), failed]
+        row2 = [word_time, start_word, rrow.mean(), rrow.min(), rrow.max(), len(failed), failed]
 
-            df2.loc[len(df2.index)] = row2
+        df2.loc[len(df2.index)] = row2
 
-        time_stop_perm = datetime.datetime.now()
-        print(f"Took {time_stop_perm-time_start_perm} seconds to complete the permutations")
+    time_stop_perm = datetime.datetime.now()
+    print(f"Took {time_stop_perm-time_start_perm} seconds to complete the permutations")
 
-        # df.sort_values(by=["Odds", ""], ascending=False, inplace=True, ignore_index=True)
-        df2.sort_values(by=["Average Score", "Failure Count"], ascending=True, inplace=True, ignore_index=True)
+    # df.sort_values(by=["Odds", ""], ascending=False, inplace=True, ignore_index=True)
+    df2.sort_values(by=["Average Score", "Failure Count"], ascending=True, inplace=True, ignore_index=True)
 
-        df2.to_csv(path_or_buf=f"{RTDIR}/../data/permutation_{method}_stats.csv", index=False)
+    df2.to_csv(path_or_buf=f"{RTDIR}/../data/permutation_{method}_stats.csv", index=False)
 
-        print(df2)
+    print(df2)
 
 
 if __name__ == "__main__":
-    t1 = Tester()
+    t1 = SimPlayer()
 
-    # t1.permutations(method='cum')
-    # t1.permutations(method='uni')
-    # t1.permutations(method='slo')
-    # t1.permutations(method='tot')
-    # print(t1.play(start="caste", solution="toxin", manual=True))
+    play()
+    permutate
+
+    # permutate(method='cum')
+    # permutate(method='uni')
+    # permutate(method='slo')
+    # permutate(method='tot')
+    # print(play(start="caste", solution="toxin", manual=True))
     # print(t1.play(start="flash", solution="mayor", manual=True, method='slo'))
     print(t1.play(start="flash", solution=None, manual=True, method='slo'))
